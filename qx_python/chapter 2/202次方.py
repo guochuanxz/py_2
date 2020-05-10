195d3631f2712349fd664b1fcde9f1c8ee3e6cb9
damon_gol <1038305340@qq.com>
2020-05-10 19:51:14 +0800

    本地库操作

---
+++ qx_python/chapter 2/202次方.py
@@ -0,0 +1,15 @@
+__author__ = 'Damon'
+#coding=utf-8
+
+# 立方
+num = 3 ** 3
+print(num)
+
+
+
+# 计算正方形的面积
+a = input("请输入正方形的边长[单位cm按回车键计算面积]：")
+square = int(a) ** 2
+print("您的正方形面积为%s平方厘米" % square)
+
+
